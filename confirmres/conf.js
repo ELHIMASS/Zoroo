2de54c39d3c94d1bd6dad1ccd924ab3c1d399719
ELHIMASS <ismail.elhimass@gmail.com>
2025-01-10 11:36:11 +0100

    changes

--- confirmres/conf.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    // Simuler une redirection
-    window.location.href = 'confirmation.html'; // Remplacez par l'URL de la page de confirmation
-});
